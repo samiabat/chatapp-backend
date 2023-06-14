a3fb995372dfe66aa25f1815199ade81e009c0ce
unknown <samuelabatneh20@gmail.com>
2023-06-14 14:32:55 +0300

    add swagger functionality

--- ChatApp.WebApi/Program.cs
+++ ChatApp.WebApi/Program.cs
@@ -1,11 +1,22 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+AddSwaggerDoc(builder.Services);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(o =>
+{
+    o.AddPolicy("CorsPolicy",
+        builder => builder.AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader());
+});
 
 var app = builder.Build();
 
@@ -16,6 +27,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+
+app.UseCors("CorsPolicy");
+app.UseAuthentication();
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Rideshare.WebApi v1"));
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
@@ -23,3 +39,45 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+void AddSwaggerDoc(IServiceCollection services)
+{
+    services.AddSwaggerGen(c => {
+        c.SwaggerDoc("v1", new OpenApiInfo
+        {
+            Title = "Samuel Chatapp API",
+            Version = "v1",
+            Description = "Samuel Chatapp API Services",
+            Contact = new OpenApiContact
+            {
+                Name = "Samuel Abatneh",
+                Email = "samuelabatneh21@gmail.com"
+            },
+        });
+        c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
+        c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        {
+            Name = "Authorization",
+            Type = SecuritySchemeType.ApiKey,
+            Scheme = "Bearer",
+            BearerFormat = "JWT",
+            In = ParameterLocation.Header,
+            Description = "JWT Authorization header using the Bearer scheme."
+        });
+
+        c.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                },
+                new string[] {}
+            }
+        });
+    });
+}
---
+++ ChatApp.WebApi/constants.cs
@@ -0,0 +1,10 @@
+﻿namespace ChatApp.WebApi
+{
+    public class constants
+    {
+        string dbname = Environment.GetEnvironmentVariable("DATABASE_NAME");
+        string dbPassword = Environment.GetEnvironmentVariable("PASSWORD");
+        string dbHost = Environment.GetEnvironmentVariable("HOST");
+        string dbUser = Environment.GetEnvironmentVariable("USER");
+    }
+}
